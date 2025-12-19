9d28ab2baafd86d9ee12e985990408c926f44896
Banka <banka@Arturss-MacBook-Air-4.local>
2025-12-19 07:07:48 +0200

    Initial commit

---
+++ AM24123_main.py
@@ -0,0 +1,8 @@
+# This is a simple calculation program
+# created to demonstrate version control.
+from AM24123_libadd import *
+from AM24123_libmult import *
+a = 13
+b = 5
+print(plus(a,b))
+print(mult(a,b))
